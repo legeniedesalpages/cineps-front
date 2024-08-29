059fc538b00aa6bf6cd244e7d5ea37ca2891651c
Renaud <renaud_balu@hotmail.com>
2024-08-29 21:21:52 +0200

    deploiement variables

---
+++ front/src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+/**
+    * @description      : 
+    * @author           : renau
+    * @group            : 
+    * @created          : 29/08/2024 - 21:20:18
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 29/08/2024
+    * - Author          : renau
+    * - Modification    : 
+**/
+export const environment = {
+    production: true,
+    title: 'CinePS',
+    apiURL: 'https://www.lgda.fr/cinepsback',
+    assetsURL: 'https://www.lgda.fr/cineps/',
+}
---
+++ front/src/environments/environment.ts
@@ -0,0 +1,18 @@
+/**
+    * @description      : 
+    * @author           : renau
+    * @group            : 
+    * @created          : 29/08/2024 - 21:20:13
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 29/08/2024
+    * - Author          : renau
+    * - Modification    : 
+**/
+export const environment = {
+    production: false,
+    title: 'CinePS Dev',
+    apiURL: 'http://localhost:8000',
+    assetsURL: 'http://localhost:5000/'
+}
